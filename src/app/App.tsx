2601df79562bb112364ef174dc691cd546ed000a
frodkata <frodkata@gmail.com>
2024-12-12 11:17:23 +0200

    Add cssbaseline

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,12 +5,14 @@ import Home from "../components/Home";
 import AuthRoute from "../components/AuthRoute";
 import { useSelector } from "react-redux";
 import { selectUser } from "../store/reducer-slices";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
 	const user = useSelector(selectUser);
 
 	return (
 		<div className="App">
+			<CssBaseline />
 			<Routes>
 				<Route path="/" element={<Login />} />
 				<Route
